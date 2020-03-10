ec06f9a80973f119ed02124dfd0f3accd9ba02b4
Andrew Polican <adpolican@gmail.com>
2020-03-09 22:17:30 -0700

    First working commit

--- main.py
+++ main.py
@@ -2,16 +2,13 @@ import networkx as nx
 from itertools import combinations as combos
 import numpy as np
 from typing import List, Tuple
+from multiprocessing import Pool
+import time
 
 
-DIMS = [10, 1]
+DIMS = [3, 3, 3]
 
-
-def mapping(x):
-    return x
-
-
-foxhole_graph = nx.relabel_nodes(nx.grid_graph(DIMS), mapping)
+foxhole_graph = nx.grid_graph(DIMS)
 nodes = sorted(list(foxhole_graph))
 corner = nodes[0]
 next_to_corner = nodes[1]
@@ -27,16 +24,6 @@ def findReducingSet(
         top_reachable_idxs,
         top_layer, bot_layer,
         parent_graph):
-    '''
-    top_reachable is a list of indices
-    top_layer should be a subgraph of a component
-    bot_layer should be a full compenent
-    '''
-    '''
-    def l2(x):
-        return sum(top_layer[x])
-    top_reachable_idxs = sorted(top_reachable_idxs, key=l2)
-    '''
     top_reachable_nodes = [v for idx, v in enumerate(top_layer)
                            if idx in top_reachable_idxs]
     layer_graph = nx.subgraph(parent_graph, top_reachable_nodes + bot_layer)
@@ -44,9 +31,7 @@ def findReducingSet(
     fraction_reachable_top = (len(top_reachable_idxs) - 0.1)/len(top_layer)
     for check_count in range(1, len(top_reachable_nodes)+1):
         lowest = 10**6
-        '''
-        print('Check count---------------', check_count)
-        '''
+        result = []
         for top_subset_idxs in combos(top_reachable_idxs, check_count):
             top_remaining = [top_layer[idx] for idx in top_reachable_idxs
                              if idx not in top_subset_idxs]
@@ -60,26 +45,18 @@ def findReducingSet(
             top_isolates = [i for i in isolates_after
                             if i in top_layer and i not in top_subset_idxs]
 
-            '''
-            print('top_reachable_idxs', top_reachable_idxs)
-            print('top_subset_idxs', top_subset_idxs)
-            print('top_remaining', top_remaining)
-            print('top_layer', top_layer)
-            print('bot_layer', bot_layer)
-            print('reachable_bot', reachable_bot)
-            print('fraction_reachable_bot', fraction_reachable_bot)
-            print('fraction_reachable_top', fraction_reachable_top)
-            '''
             if any(top_isolates):
                 msg = 'Isolates in the top_layer of induced subgraph'
                 raise GraphError(msg)
 
-            if fraction_reachable_bot < fraction_reachable_top:
-                # if fraction_reachable_bot < lowest):
+            if fraction_reachable_bot <= fraction_reachable_top:
+                if fraction_reachable_bot < lowest:
+                    result = []
+                    lowest = fraction_reachable_bot
                 bot_reachable_idxs = [bot_layer.index(v)
                                       for v in reachable_bot]
-                result = check_count, sorted(bot_reachable_idxs)
-        if result is not None:
+                result.append((check_count, sorted(bot_reachable_idxs)))
+        if result:
             break
 
     if result is None:
@@ -97,55 +74,63 @@ if __name__ == '__main__':
     checkGraph = nx.DiGraph()
     # vertices are (part number, index tuple)
     # edges have check count as weight
+    PART_0 = 0
+    PART_1 = 1
 
-    checkGraph.add_edge('START', ('part_0', tuple(top_reachable_idxs)), weight=0)
-    checkGraph.add_edge('START', ('part_1', tuple(bot_reachable_idxs)), weight=0)
+    checkGraph.add_edge('START', (PART_0, tuple(top_reachable_idxs)), weight=0)
+    checkGraph.add_edge('START', (PART_1, tuple(bot_reachable_idxs)), weight=0)
+
+    edges = []
 
     top = part_0
     bot = part_1
+    start = time.time()
     for checkCount in range(1, len(top) + 1):
         for checkIdxs in combos(range(len(top)), checkCount):
-            _ = findReducingSet(checkIdxs, top, bot, foxhole_graph)
-            min_checks, bot_reachable_idxs = _
-            origin = ('part_0', tuple(checkIdxs))
-            dest = ('part_1', tuple(bot_reachable_idxs))
-            weight = min_checks
-            if not bot_reachable_idxs:
-                dest = 'END'
-            print(origin, dest, weight)
-            checkGraph.add_edge(origin, dest, weight=weight)
+            checks_and_idxs = findReducingSet(checkIdxs, top, bot, foxhole_graph)
+            for min_checks, bot_reachable_idxs in checks_and_idxs:
+                origin = (PART_0, tuple(checkIdxs))
+                dest = (PART_1, tuple(bot_reachable_idxs))
+                weight = min_checks
+                if not bot_reachable_idxs:
+                    dest = 'END'
+                edges.append((origin, dest, {'weight': weight}))
+    end = time.time()
+    print(f'checkCount: {end-start:.5}')
 
     top = part_1
     bot = part_0
+    start = time.time()
     for checkCount in range(1, len(top) + 1):
         for checkIdxs in combos(range(len(top)), checkCount):
-            _ = findReducingSet(checkIdxs, top, bot, foxhole_graph)
-            min_checks, bot_reachable_idxs = _
-            origin = ('part_1', tuple(checkIdxs))
-            dest = ('part_0', tuple(bot_reachable_idxs))
-            weight = min_checks
-            if not bot_reachable_idxs:
-                dest = 'END'
-            print(origin, dest, weight)
-            checkGraph.add_edge(origin, dest, weight=weight)
+            checks_and_idxs = findReducingSet(checkIdxs, top, bot, foxhole_graph)
+            for min_checks, bot_reachable_idxs in checks_and_idxs:
+                origin = (PART_1, tuple(checkIdxs))
+                dest = (PART_0, tuple(bot_reachable_idxs))
+                weight = min_checks
+                if not bot_reachable_idxs:
+                    dest = 'END'
+                edges.append((origin, dest, {'weight': weight}))
+    end = time.time()
+    print(f'checkCount: {end-start:.5}')
+
+    checkGraph.add_edges_from(edges)
+
+    all_edges = checkGraph.edges()
 
     sp = nx.algorithms.shortest_paths.generic.shortest_path
-    path = sp(checkGraph, 'START', 'END', weight='weight')
-    print('RESULT:', path)
-    for origin, dest in zip(path, path[1:]):
-        print(checkGraph[origin][dest]['weight'])
-
-    
-
-    '''
-    top = part_0
-    bot = part_1
-    while not done:
-        _ = findReducingSet(top_reachable_idxs, top, bot, foxhole_graph)
-        top_check_idxs, bot_reachable_idxs = _
-        top_reachable_idxs = bot_reachable_idxs
-        done = not bot_reachable_idxs
-        all_checks.append(top_check_idxs)
-        top, bot = bot, top
-    print(f'{max(all_checks)} checks per day')
-    '''
+    max_checks = max([checkGraph[u][v]['weight'] for u, v in all_edges])
+    for checks in range(max_checks + 1):
+        sub_edges = [(u, v) for u, v in all_edges
+                     if checkGraph[u][v]['weight'] <= checks]
+        sub_graph = checkGraph.edge_subgraph(sub_edges)
+        try:
+            path = sp(sub_graph, 'START', 'END', weight='weight')
+            checkCount = 0
+            for origin, dest in zip(path, path[1:]):
+                checkCount = max(checkCount, checkGraph[origin][dest]['weight'])
+            print('Fox hole graph dimensions', DIMS)
+            print(f'Result: {checkCount} check{"s" if (checkCount-1) else ""} per day')
+            break
+        except:
+            pass
